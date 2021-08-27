86d528ef6fdac4743a26c953e015f121d12f0708
492988 <492988@mail.muni.cz>
2021-08-27 19:43:47 +0200

    remove python django project

--- dictators/dictators_game/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from dictators.dictators_game import views
-
-urlpatterns = [
-    path("api/user/create", views.CreateUser.as_view()),
-    path("api/user/delete", views.DeleteUser.as_view()),
-    path("api/user/authenticate", views.AuthenticateUser.as_view()),
-    path("api/leaderboard", views.Leaderboard.as_view())
-]
--- dictators/dictators_project/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import include, path, re_path
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('dictators.dictators_game.urls'))
-]
